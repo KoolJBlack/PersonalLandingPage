b491cde984e009ddf41d8cfdf62f00b8564ab7e0
KoolJBlack <kooolj@gmail.com>
2013-01-06 15:57:46 -0500

    before fancy box

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
 		$('#slideshow').cycle({
-		fx: 'fade',
-		pager: '#smallnav', 
-		pause:   1, 
-		speed: 1800,
-		timeout:  3000,
+		fx: 'fade', 
+		speed: 3000,
+		timeout: 10000,
 		random: 1 
 	});			
 });
